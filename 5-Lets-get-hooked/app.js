312ae6b8fc76481807b0eec3c5a9ef07f7aae29d
Ankit Mishra <ankit.mishra@jungleegames.com>
2023-01-15 11:44:45 +0530

    Lets Get Hooked

--- 5-Lets-get-hooked/app.js
+++ 5-Lets-get-hooked/app.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
 
+
 const AppLayout = () => {
     return <React.Fragment>
         <Header />
--- 5-Lets-get-hooked/components/Body.js
+++ 5-Lets-get-hooked/components/Body.js
@@ -2,9 +2,28 @@ import SearchBar from "./SearchBar";
 import RestaurantCard from "./RestaurantCard";
 import { restaurantss } from "../constants";
 import React,{ useState, useEffect } from "react";
+import { Image, Shimmer } from 'react-shimmer'
+
+const ShimmerContent = () => {
+    return <>
+    <div className="container">
+                  
+                    <div className=" cont-body">
+                    
+                    {[1,2,3,4,5,6,7,8,9,0].map((elem, index) => {
+                        return <div className="shimmi" fallback={<Shimmer width={250} height={250} />}></div>
+                    //     return  <Image key={index} className="shimmi"
+                    //     src='https://source.unsplash.com/random/800x600'
+                    //     fallback={<Shimmer width={250} height={250} />}
+                    //   />
+                    })}
+                    </div>
+                </div>
+    </>
+}
 
 const Body = () => {
-    const [filteredRestaurants, setFilteredRestaurants] = useState(restaurantss[0].data.data.cards);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
     const [allRestaurants, setAllRestaurants] = useState([]);
 
     const getRestaurantData = async () => {
@@ -27,7 +46,7 @@ const Body = () => {
                     <SearchBar filteredRestaurants={filteredRestaurants} allRestaurants={allRestaurants} changeRestaurantList={changeRestaurantList}  />
                     <div className=" cont-body">
                     {/* {console.log(restaurants[0].data.data.cards)} */}
-                    {filteredRestaurants.map((restaurant) => {
+                    {filteredRestaurants.length == 0 ? <ShimmerContent /> :  filteredRestaurants.map((restaurant) => {
                         return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
                     })}
                     </div>
